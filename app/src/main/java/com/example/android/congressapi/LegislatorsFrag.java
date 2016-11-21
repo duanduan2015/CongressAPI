86245797f881da27d4a4ad7f7ab1fdf766791769
Yunduan Han <yunduan.han@gmail.com>
2016-11-21 00:16:46 -0800

    ListView works

--- app/src/main/java/com/example/android/congressapi/LegislatorsFrag.java
+++ app/src/main/java/com/example/android/congressapi/LegislatorsFrag.java
@@ -1,27 +1,45 @@
 package com.example.android.congressapi;
 
 import android.app.Fragment;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by duanduan on 11/20/16.
  */
 public class LegislatorsFrag extends Fragment {
+    private ArrayAdapter<String> listAdapter ;
+    private String[] mPlanetTitles = new String[]{"a", "b", "c", "d", "e"};
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
         getActivity().setTitle("Legislators");
-        return inflater.inflate(
-                R.layout.legislators, container, false);
+        View view = inflater.inflate(R.layout.legislators, container, false);
+        ListView mDrawerList  = (ListView) view.findViewById(R.id.legislatorsList);
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars","Jupiter", "Saturn", "Uranus", "Neptune"};
+        ArrayList<String> planetList = new ArrayList<String>();
+        planetList.addAll(Arrays.asList(planets));
+        listAdapter = new ArrayAdapter<String>(getActivity(), R.layout.legislators_item, planetList);
+        listAdapter.add( "Ceres" );
+        listAdapter.add( "Pluto" );
+        listAdapter.add( "Haumea" );
+        listAdapter.add( "Makemake" );
+        listAdapter.add( "Eris" );
+        mDrawerList.setAdapter(listAdapter);
+        return view;
     }
     /*public void updateLegislators() {
         getActivity().setTitle("Legislators");
     }*/
+
 }
 
