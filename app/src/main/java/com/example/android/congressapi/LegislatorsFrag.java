7a9c6c4aa13d0b4ba938f8d688b39548e4e50fcc
Yunduan Han <yunduan.han@gmail.com>
2016-11-22 12:10:49 -0800

    http request works using volley

--- app/src/main/java/com/example/android/congressapi/LegislatorsFrag.java
+++ app/src/main/java/com/example/android/congressapi/LegislatorsFrag.java
@@ -4,9 +4,11 @@ package com.example.android.congressapi;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -23,6 +25,21 @@ import android.widget.ListView;
 import android.view.View.OnClickListener;
 import android.widget.Toast;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -30,6 +47,7 @@ import java.util.Arrays;
  * Created by duanduan on 11/20/16.
  */
 public class LegislatorsFrag extends Fragment {
+    private JSONObject legislators;
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
@@ -64,10 +82,55 @@ public class LegislatorsFrag extends Fragment {
 
             }
         });
+        String[] urls = new String[3];
+        urls[0] = "http://homework8-148505.appspot.com/main.php?query=legislators";
+        GetJasonData getter = new GetJasonData();
+        getter.doInBackground(urls);
+        //getter.onPostExecute(legislators);
+        /*JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, urls[0], null, new Response.Listener<JSONObject>() {
 
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        legislators = response;
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(getContext()).addToRequestQueue(jsObjRequest);*/
         return view;
     }
+    private class GetJasonData extends AsyncTask<String, Integer, Long> {
+        protected Long doInBackground(String[] urls) {
+            int count = urls.length;
+            long totalSize = 0;
+            JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                    (Request.Method.GET, urls[0], null, new Response.Listener<JSONObject>() {
 
+                        @Override
+                        public void onResponse(JSONObject response) {
+                           legislators = response;
+                            Log.i("data", legislators.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // TODO Auto-generated method stub
+
+                        }
+                    });
+
+            // Access the RequestQueue through your singleton class.
+            MySingleton.getInstance(getContext()).addToRequestQueue(jsObjRequest);
+            return totalSize;
+        }
+    }
 
     public static class TabFragment1 extends Fragment {
         //private ArrayAdapter<String> listAdapter ;
